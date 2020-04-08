a348e6dd21c9c797ca03f87a564277d73edba2df
Shohruh <shohruh@3i.ai>
2020-04-08 17:17:03 +0500

    room ui changes

--- public/scripts/room.js
+++ public/scripts/room.js
@@ -1,15 +1,27 @@
-
-const userData = JSON.parse(localStorage.getItem('user'))
-const roomData = JSON.parse(localStorage.getItem('room'))
+const userData = JSON.parse(localStorage.getItem('user'));
+const roomData = JSON.parse(localStorage.getItem('room'));
 
 const roomSocket = io(window.location.origin);
-
+console.log(userData);
 roomSocket.on('connect', () => {
-  console.log('socket connected');
+  console.log('socket connected', 'room js');
   roomSocket.on('joined', (data) => {
     console.log(data);
   })
 });
 
 
-roomSocket.emit('join', {msg: 'hello', roomId: roomData.id});
+roomSocket.emit('join', {
+  msg: 'hello',
+  roomId: roomData.id
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('roomName').innerHTML = roomData.name;
+});
+
+function endCall() {
+  roomSocket.disconnect();
+  window.location.href = '/users/register'
+}
